e4ef60863bd81969fa8c063f8fac02708c6d0e21
Tingting Zhao <RexRowan@users.noreply.github.com>
2016-06-10 16:23:25 +0100

    Create sb.py

---
+++ sb.py
@@ -0,0 +1,12 @@
+def stirling(n ,k):
+    if n < k:
+        return 0
+    if n == k or k == 1:
+    return 1
+  return k*stirling(n-1, k) + stirling(n-1, k-1)  
+  
+def bell(n):
+    total = 0
+    for k in range(1, n+1):
+        total = total + stirling(n, k)
+    return total  
